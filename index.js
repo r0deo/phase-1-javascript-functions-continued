d44324f8a69b2c909e036ab54d946cf255461d6f
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-02-24 11:05:04 +0300

    completed by Ali

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+// Function that returns a String for Saturday activities
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function that returns a String for Monday work
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Function that returns a function with visual flair for wrapping adjectives
+  function wrapAdjective(flair = '*') {
+    // Inner function that takes an adjective and wraps it in visual flair
+    return function (adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); // "You are !!!amazing!!!"
+  console.log(encouragingPromptFunction()); // "You are !!!special!!!"
+  
